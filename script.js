dabb0bbecca4c5cd76710932e7fc99b361d454ef
dorakkj <doravitoria77@gmail.com>
2025-03-13 21:25:34 -0300

    v

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function mostrarMateria(materia) {
-    // Esconde todas as seções
-    const materias = document.querySelectorAll('.materia');
-    materias.forEach(materia => {
-        materia.style.display = 'none';
-    });
 
-    // Exibe a seção clicada
-    const materiaSelecionada = document.getElementById(materia);
-    if (materiaSelecionada) {
-        materiaSelecionada.style.display = 'block';
-    }
-}
