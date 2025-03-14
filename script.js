f75ca2c0bb1971fbd03827abd312953d483eb557
dorakkj <doravitoria77@gmail.com>
2025-03-13 21:18:09 -0300

    n

---
+++ script.js
@@ -0,0 +1,9 @@
+// Rolagem suave para os links de navegação
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
